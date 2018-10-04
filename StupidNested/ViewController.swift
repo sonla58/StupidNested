88d5a5418472e934bccd3320f1f068030cc0c082
Anh Son Le <leanhson.uet@gmail.com>
2018-10-04 16:11:57 +0700

    Initial Commit

---
+++ StupidNested/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StupidNested
+//
+//  Created by Anh Son Le on 10/4/18.
+//  Copyright © 2018 Anh Son Le. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
